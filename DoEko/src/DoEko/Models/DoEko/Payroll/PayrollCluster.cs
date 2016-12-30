7c9a71465dacca2a34f369cf161430c8a11cb466
Wojciech Gorniak <wojciech.gorniak@outlook.com>
2016-12-30 09:34:52 +0100

    poprawka do listy inwestycji - minify js files poprawka do wgladu inwestycji - disabled buttons when in approval mode

---
+++ DoEko/src/DoEko/Models/DoEko/Payroll/PayrollCluster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoEko.Models.DoEko.Payroll
+{
+    public class PayrollCluster
+    {
+        [Key()]
+        public Guid PayrollClusterId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime ChangedAt { get; set; }
+        public Guid ChangedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        
+        [ForeignKey("WageType")]
+        public int WageTypeId { get; set; }
+        public decimal WageAmount { get; set; }
+        public Guid WageReference { get; set; }
+        public virtual WageType WageType { get; set; }
+    }
+}
