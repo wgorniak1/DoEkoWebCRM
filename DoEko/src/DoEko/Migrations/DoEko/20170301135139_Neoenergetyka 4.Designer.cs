ad2bdfa581389c4bf7cd3bf572d4cf5393b415c3
Wojciech Gorniak <wojciech.gorniak@outlook.com>
2017-03-01 14:52:31 +0100

    neoenergetyka 4

--- DoEko/src/DoEko/Migrations/DoEko/20170301135139_Neoenergetyka 4.Designer.cs
+++ DoEko/src/DoEko/Migrations/DoEko/20170301135139_Neoenergetyka 4.Designer.cs
@@ -8,9 +8,10 @@ using DoEko.Models.DoEko;
 namespace DoEko.Migrations.DoEko
 {
     [DbContext(typeof(DoEkoContext))]
-    partial class DoEkoContextModelSnapshot : ModelSnapshot
+    [Migration("20170301135139_Neoenergetyka 4")]
+    partial class Neoenergetyka4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
